2546614e514e79e26c0c41b3b5ceb10ea3d032e0
Kishor C <kishor1124@gmail.com>
2026-02-17 20:43:27 +0530

    spring boot data jpa

--- examples/spring-jpa-demo.zip_expanded/spring-jpa-demo/src/main/java/com/example/spring_jpa_demo/ProfileController.java
+++ examples/spring-jpa-demo.zip_expanded/spring-jpa-demo/src/main/java/com/example/spring_jpa_demo/ProfileController.java
@@ -1,11 +1,15 @@
 package com.example.spring_jpa_demo;
 
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -29,5 +33,38 @@ public class ProfileController {
 	public ResponseEntity<Object> findProfiles() {
 		List<Profile> profiles = profileService.fetchProfiles();
 		return ResponseEntity.status(200).body(profiles);
+	}
+	// /return the profile or error in JSON
+	@GetMapping(path = "/profile/{id}")
+	public ResponseEntity<Object> findProfile(@PathVariable("id") int profileId) {
+		try {
+			Profile profile = profileService.fetchProfile(profileId);
+			return ResponseEntity.status(200).body(profile);
+		} catch(ProfileNotFoundException e) {
+			Map<String, String> map = Map.of("message", e.getMessage());
+			return ResponseEntity.status(404).body(map);
+		}
+	}
+	// deleting the profile based on id
+	@DeleteMapping(path = "/profile/{id}")
+	public ResponseEntity<Object> removeProfile(@PathVariable("id") int profileId) {
+		try {
+			profileService.deleteProfile(profileId);
+			return ResponseEntity.status(200).body("Profile delete successfully");
+		} catch(ProfileNotFoundException e) {
+			Map<String, String> map = Map.of("message", e.getMessage());
+			return ResponseEntity.status(404).body(map);
+		}
+	}
+	// this updates the profile based on the id
+	@PutMapping(path = "/profile/{id}")
+	public ResponseEntity<Object> editProfile(@PathVariable("id") int profileId, @RequestBody Profile profile) {
+		try {
+			Profile p = profileService.updateProfile(profileId, profile);
+			return ResponseEntity.status(200).body(p);
+		} catch(ProfileNotFoundException e) {
+			Map<String, String> map = Map.of("message", e.getMessage());
+			return ResponseEntity.status(404).body(map);
+		}
 	}
 }
--- examples/spring-jpa-demo.zip_expanded/spring-jpa-demo/src/main/java/com/example/spring_jpa_demo/ProfileService.java
+++ examples/spring-jpa-demo.zip_expanded/spring-jpa-demo/src/main/java/com/example/spring_jpa_demo/ProfileService.java
@@ -1,6 +1,7 @@
 package com.example.spring_jpa_demo;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -25,6 +26,36 @@ public class ProfileService {
 	 * there are built-in methods like save(T), findAll
 	 * to store & retrieve 
 	 */
+	
+	// findById that 
+	public Profile fetchProfile(int id) throws ProfileNotFoundException {
+		// findById returns Optional, some methods it has orElse, orElseThrow, isPresent
+		Optional<Profile> option = profileDao.findById(id);
+		return option.orElseThrow(()->new ProfileNotFoundException("Profile not found for an id "+id));
+	}
+	// delete by id
+	@Transactional
+	public void deleteProfile(int id) throws ProfileNotFoundException {
+		Profile p = fetchProfile(id);
+		profileDao.delete(p);
+	}
+	
+	// update profile based on the id
+	@Transactional
+	public Profile updateProfile(int id, Profile profile) throws ProfileNotFoundException{
+		Profile p = fetchProfile(id);
+		if(profile.getPhone() != 0) {
+			p.setPhone(profile.getPhone());
+		}
+		if(profile.getName() != null) {
+			p.setName(profile.getName());
+		}
+		if(profile.getDob() != null) {
+			p.setDob(profile.getDob());
+		}
+		return p;
+	}
+	
 	// save method saves an entity and returns the saved entity
 	// if id is auto-generated you can view that id
 	@Transactional // required when you save, delete, update
